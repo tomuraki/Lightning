b8fa100b8e9f55c8020dd8052413f6fe35dab732
unknown <tomuraki@697-334-Bilbo.sf.k12.internal>
2015-09-03 08:08:18 -0700

    9.3 3

--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Lightning extends PApplet {
+
   int startX=(int)(Math.random()*300);
   int startY=0;
   int endX=(int)(Math.random()*300);
@@ -5,13 +21,13 @@
   int cx=25;
 
 
-void setup()
+public void setup()
 {
 	size(300,300);
 	background(0,0,0);
-	strokeWeight(.75);
+	strokeWeight(.75f);
 }
-void draw()
+public void draw()
 {
 	stroke((int)(Math.random()*255), (int)(Math.random()*255), (int)(Math.random()*255));
 	while(startY<310)
@@ -27,7 +43,7 @@ void draw()
 	
 }
 
-void mousePressed()
+public void mousePressed()
 {
 	startX=(int)(Math.random()*300);
 	startY=0;
@@ -37,7 +53,7 @@ void mousePressed()
 }
 
 
-void cloud()
+public void cloud()
 {
 	noStroke();
 	fill(50,50,50);
@@ -55,3 +71,12 @@ void cloud()
 
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Lightning" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
